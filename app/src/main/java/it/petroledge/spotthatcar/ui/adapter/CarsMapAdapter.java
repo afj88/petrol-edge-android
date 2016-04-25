30adedbc59bfb6e7bfc190a6f9e2830416bb8ac0
Alessandro Frigerio <alessandro.frigerio88@gmail.com>
2016-04-25 23:59:19 +0200

    Switch to external Map Adapter

---
+++ app/src/main/java/it/petroledge/spotthatcar/ui/adapter/CarsMapAdapter.java
@@ -0,0 +1,123 @@
+package it.petroledge.spotthatcar.ui.adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.petroledge.spotthatcar.R;
+import it.petroledge.spotthatcar.entity.CarEntity;
+import it.petroledge.spotthatcar.manager.LoadedCarsEvent;
+
+/**
+ * Created by friz on 22/04/16.
+ */
+public class CarsMapAdapter implements OnMapReadyCallback, GoogleMap.InfoWindowAdapter {
+
+    private final Context mContext;
+    private final View mInfoWindow;
+
+    private GoogleMap mMap;
+    private Map<String, String> mMarkerIdsMap;
+    private String mLastId;
+    private boolean mIsMapReady = false;
+    private List<CarEntity> mCarEntityList;
+//    private Set<PicassoMarker> markerSet = new HashSet<>();
+
+    public CarsMapAdapter(Context context) {
+        mContext = context;
+        mInfoWindow = LayoutInflater.from(context).inflate(R.layout.bubble_marker_view, null);
+        mMarkerIdsMap = new HashMap<>();
+
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setInfoWindowAdapter(this);
+
+        mIsMapReady = true;
+
+        drawOnMap();
+    }
+
+    @Subscribe
+    public void onLoadedCarsEvent(LoadedCarsEvent event) {
+
+        mCarEntityList = event.getPhotos();
+
+        drawOnMap();
+
+    }
+
+    private void drawOnMap() {
+
+        if (mIsMapReady && mCarEntityList != null) {
+            mMarkerIdsMap = new HashMap<>();
+            mMap.clear();
+
+            for (CarEntity pe : mCarEntityList) {
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(pe.getPosition());
+                markerOptions.title(pe.getName());
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.black_marker));
+                Marker marker = mMap.addMarker(markerOptions);
+
+                mMarkerIdsMap.put(marker.getId(), pe.getImageURI());
+            }
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mCarEntityList.get(0).getPosition(), 15));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        }
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        if (marker.getId().equals(mLastId))
+            return  mInfoWindow;
+
+        mLastId = marker.getId();
+        String uri = mMarkerIdsMap.get(marker.getId());
+
+        ImageView iwPhotoHolder = (ImageView) mInfoWindow.findViewById(R.id.info_window_photo_holder);
+//        if (iwPhotoHolder.getDrawable() == null) {
+            Picasso.with(mContext).load(uri).into(iwPhotoHolder, new Callback() {
+                @Override
+                public void onSuccess() {
+                    mInfoWindow.findViewById(R.id.info_window_loader).setVisibility(View.GONE);
+//                    marker.showInfoWindow();
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+//        }
+        return mInfoWindow;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
--- app/src/main/java/it/petroledge/spotthatcar/ui/view/PicassoMarker.java
+++ app/src/main/java/it/petroledge/spotthatcar/ui/view/PicassoMarker.java
@@ -11,12 +11,14 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import it.petroledge.spotthatcar.App;
 import it.petroledge.spotthatcar.R;
 
 public class PicassoMarker implements Target {
@@ -58,13 +60,39 @@ public class PicassoMarker implements Target {
 
     private Bitmap getMarkerBitmapFromView(Bitmap bmp1) {
 
-        Bitmap bmp2 = BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.bubble);
+        Bitmap bmp2 = BitmapFactory.decodeResource(App.getAppContext().getResources() , R.drawable.bubble_extra_small);
 
         Bitmap bmOverlay = Bitmap.createBitmap(bmp2.getWidth(), bmp2.getHeight(), bmp2.getConfig());
         Canvas canvas = new Canvas(bmOverlay);
-        canvas.drawBitmap(bmp1, new Matrix(), null);
+        Bitmap resized = getResizedBitmap(bmp1, bmp2.getWidth() * 0.83, bmp2.getHeight() * 0.7);
+
+        Matrix tMatrix = new Matrix();
+        tMatrix.setTranslate((float)(bmp2.getWidth() * 0.088), (float)(bmp2.getHeight() * 0.096));
+
+        canvas.drawBitmap(resized, tMatrix, null);
         canvas.drawBitmap(bmp2, new Matrix(), null);
 
         return bmOverlay;
+    }
+
+
+    private Bitmap getResizedBitmap(Bitmap bm, double newWidth, double newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+
+        //ThumbnailUtils.extractThumbnail(bm, width, height,  )
+
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+
+        return resizedBitmap;
     }
 }
